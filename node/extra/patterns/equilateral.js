15ce928492a21424d5d0788fb1f46b82d5252354
abhishek960528 <abhishek.rakhade@successive.tech>
2021-04-27 14:32:50 +0530

    feat: intro to Node.js & NPM

---
+++ node/extra/patterns/equilateral.js
@@ -0,0 +1,26 @@
+function createSpaces(n) {
+  var spaces = "";
+  for (i = 1; i < n; i++) {
+    spaces += " ";
+  }
+
+  return spaces;
+}
+
+function addStar(s) {
+  return (s += "* ");
+}
+
+function removeStar(s) {
+  return s.substring(0, s.length - 2);
+}
+
+function printequilateral(rows) {
+  var star = "";
+  for (var i = 0; i < rows; i++) {
+    var spaces = createSpaces(rows - i);
+    star = addStar(star);
+    console.log(spaces + star);
+  }
+}
+printequilateral(10);
